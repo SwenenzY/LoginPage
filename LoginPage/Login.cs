43481a2d90f263baec3b57eabf730310422fdde6
SwenenzY <44095096+SwenenzY@users.noreply.github.com>
2020-04-23 06:48:12 +0300

    Publish Files
    Publish Files

---
+++ LoginPage/Login.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginPage
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Register reg = new Register();
+            reg.Show();       
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int wins = 0;
+            string aname = null;
+            FileStream file = new FileStream(@"F:\\LoginPage\\LoginPage\\bin\\Debug\\Accounts.txt", FileMode.Open);
+            StreamReader sw = new StreamReader(file);
+            int Errorcount = 0;
+            for (int i = 0; i < file.Length; i++)
+            {
+              
+                string text = sw.ReadLine();
+                if (string.IsNullOrEmpty(text)) continue;
+                int Long = text.LastIndexOf(':');
+                string password = text.Substring(Long+1); 
+                string Username = text.Substring(0, Long);
+                string name = textBox1.Text;
+                string pass = textBox2.Text;
+                aname = name;
+                if (Username == name && pass == password)
+                {
+                    wins++;
+                      
+                }
+            }
+         
+            if (wins == 1)
+            {
+                MessageBox.Show("Hi " + aname + "!");
+            }
+            else
+            {
+                MessageBox.Show("Wrong username or password");
+            }
+
+            sw.Close();
+            file.Close();
+        }
+    }
+}
---
+++ LoginPage/Register.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginPage
+{
+    public partial class Register : Form
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Login log = new Login();
+            log.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bool alreadyhave = false;
+            string puser = textBox1.Text;
+            string ppass = textBox2.Text;
+            FileStream filex = new FileStream(@"F:\\LoginPage\\LoginPage\\bin\\Debug\\Accounts.txt", FileMode.Open);
+            StreamReader swx = new StreamReader(filex);
+            for (int i = 0; i < filex.Length; i++)
+            {
+
+                string textx = swx.ReadLine();
+                if (string.IsNullOrEmpty(textx)) continue;
+                int Long = textx.LastIndexOf(':');
+                string password = textx.Substring(Long + 1);
+                string Username = textx.Substring(0, Long);
+                if (puser == Username)
+                {
+                    alreadyhave = true;
+                }
+            }
+            swx.Close();
+            filex.Close();
+            
+            string text = File.ReadAllText(@"F:\\LoginPage\\LoginPage\\bin\\Debug\\Accounts.txt");
+            FileStream file = new FileStream(@"F:\\LoginPage\\LoginPage\\bin\\Debug\\Accounts.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(file);
+            if (textBox2.Text.Length < 6)
+            {
+                MessageBox.Show("Error! your password lower a 6");
+                return;
+            }
+            if (alreadyhave)
+            {
+                MessageBox.Show("Error! your username already using");
+                return;
+            }
+            if (textBox1.Text == null)
+            {
+                MessageBox.Show("Error! Blank username");
+            } else if (textBox2.Text == null)
+            {
+                MessageBox.Show("Error! Blank Password");
+            }
+            else
+            {
+                string Name = textBox1.Text;
+                string Pass = textBox2.Text;
+                sw.Write(text + "\n\n" + Environment.NewLine  + Name + ":" + Pass);
+                sw.Flush();
+                MessageBox.Show("Your register as successful, you're now login! \n your redicting.... in 2 seconds");
+                Thread.Sleep(2000);
+                this.Hide();
+                Login log = new Login();
+                log.Show();
+            }
+            sw.Close();
+            file.Close();
+            Thread.Sleep(1000);
+        }
+    }
+}
